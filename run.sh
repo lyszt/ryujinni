48f19094ea0b882f58258774b59c3184e3c2a4ed
lyszt <kalliddel@gmail.com>
2025-10-08 18:01:01 -0300

    run.sh helpfully exports PATH

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export PATH="$PROJECT_DIR/vendor/ffmpeg/bin:$PATH"
+
 mix deps.update --all
 mix compile
 iex -S mix
