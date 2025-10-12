b3e722ed81c5df2784920237a15ee3e494821cb2
lyszt <kalliddel@gmail.com>
2025-10-12 01:25:07 -0300

    Preparing the gears for python integration

--- native/ryujin/src/lib.rs
+++ native/ryujin/src/lib.rs
@@ -1,6 +1,24 @@
+use pyo3::prelude::*;
+
 #[rustler::nif]
 fn add(a: i64, b: i64) -> i64 {
     a + b
 }
+
+
+// Example of python usage 
+// /// A simple function to add two numbers
+// #[pyfunction]
+// fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// /// A Python module implemented in Rust.
+// #[pymodule]
+// fn my_rust_module(_py: Python, m: &PyModule) -> PyResult<()> {
+//     m.add_function(wrap_pyfunction!(add, m)?)?;
+//     Ok(())
+// }
+
 
 rustler::init!("Elixir.Ryujin");
